cfac125e1739313742daaee3d2ffd04d8a88939c
aline <92822845+miistt@users.noreply.github.com>
2022-05-02 17:53:23 -0300

    Update exe2.php

--- exe2.php
+++ exe2.php
@@ -2,9 +2,9 @@
 
 <?php
 
-$C = 34;
-$F = (9 * $C + 160) / 5;
+$c = 34;
+$f = (9 * $c + 160) / 5;
 
-echo "A temperatura em Fahrenheit é igual a: $F";
+echo "A temperatura em Fahrenheit é igual a: $f";
 
 ?>
